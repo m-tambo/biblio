d53e54186185e1e89ac41b02a7ba760567130f26
tambo <michael_tambornino@comcast.com>
2020-06-19 11:10:55 -0400

    Test GetAuthors method

--- handler/dao_test.go
+++ handler/dao_test.go
@@ -1,15 +1,32 @@
 package handler
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http/httptest"
 	"testing"
+
+	"biblio/schema"
 )
 
 func TestGetAuthorsHandler(t *testing.T) {
 	handler := NewGetAuthorsHandler(MockDAO{})
 
 	srv := httptest.NewServer(handler)
-	srv.Start()
 	defer srv.Close()
+
+	res, err := srv.Client().Get(srv.URL)
+	if err != nil {
+		fmt.Println("errorrrr:", err)
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+
+	}
+	var un []schema.Author
+
+	json.Unmarshal(body, &un)
+	fmt.Println(un)
 
 }
--- handler/mock_dao.go
+++ handler/mock_dao.go
@@ -1,16 +1,17 @@
 package handler
 
 import (
+	"database/sql"
 	"time"
 
 	"biblio/schema"
 )
 
 type MockDAO struct {
-
+	db *sql.DB
 }
 
-func (d MockDAO) GetAuthors() []schema.Author {
+func (md MockDAO) GetAuthors() []schema.Author {
 	return []schema.Author{
 		{
 			ID:        1,
@@ -19,5 +20,20 @@ func (d MockDAO) GetAuthors() []schema.Author {
 			Dob:       "xxx",
 			Created:   time.Now(),
 		},
+		{
+			ID:        2,
+			FirstName: "maggie",
+			LastName:  "baum",
+			Dob:       "1997-3-24",
+			Created:   time.Now(),
+		},
 	}
+}
+
+func (md MockDAO) CreateAuthor() {
+
+}
+
+func (md MockDAO) DeleteAuthor(id string) {
+
 }
